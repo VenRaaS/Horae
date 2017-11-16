1fdb3920e498011367e37e95ccd76799a7236a2c
eric0329 <ericyang0329@cron-1.c.venraasitri.internal>
2017-11-17 02:55:54 +0800

    [lib] add hmessage.py and subscr.py - hmessage.py, handle horae message - subscr.py, subscription enum

---
+++ lib/subscr.py
@@ -0,0 +1,6 @@
+from enum import Enum
+
+#-- Enum - https://stackoverflow.com/questions/36932/how-can-i-represent-an-enum-in-python?page=1&tab=votes#tab-top
+EnumSubscript = Enum('EnumSubscript', 'pull_bucket_ven-custs')
+
+
