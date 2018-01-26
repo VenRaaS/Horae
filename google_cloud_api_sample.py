1ad230f89dc16fc66705fbf9852f46de6c078cec
eric0329 <ericyang0329@cron-1.c.venraasitri.internal>
2018-01-26 10:37:01 +0800

    [google_cloud_api_sample.py] refine sub/pub functions

--- google_cloud_api_sample.py
+++ google_cloud_api_sample.py
@@ -13,7 +13,6 @@ PROJECT_NAME = 'venraasitri'
 TOPIC = 'bucket_ven-custs'
 
 
-pp = pprint.PrettyPrinter(indent=4)
 
 def fqrn(resource_type, project, resource):
     """Return a fully qualified resource name for Cloud Pub/Sub."""
@@ -27,22 +26,29 @@ def get_full_subscription_name(project, subscription):
     """Return a fully qualified subscription name."""
     return fqrn('subscriptions', project, subscription)
 
-
-def publish_message(client, topic, msg):
+def publish_message(client, proj_name, topic, msg):
     """Publish a message to a given topic."""
-    topic = get_full_topic_name(PROJECT_NAME, topic)
+    try:
+        topic = get_full_topic_name(proj_name, topic)
 
-    attributes = msg['attributes']
-    data = base64.b64encode(str(msg['data']))
-    body = {'messages': [{'attributes': attributes, 'data': data}]}
+        attributes = msg['attributes']
+        data = base64.b64encode(str(msg['data']))
+        body = {'messages': [
+                    {
+                        'attributes': attributes, 
+                        'data': data
+                    }
+                ]}
 
-    resp = client.projects().topics().publish(
-        topic=topic, body=body).execute(num_retries=NUM_RETRIES)
+        resp = client.projects().topics().publish(
+            topic=topic, body=body).execute(num_retries=NUM_RETRIES)
 
-    print ('Published a message "{}" to a topic {}. The message_id was {}.'
-           .format(msg['attributes'], topic, resp.get('messageIds')[0]))
+        print ('Published a message "{}" to a topic {}. The message_id was {}.'
+               .format(msg['attributes'], topic, resp.get('messageIds')[0]))
+    except Exception as e:
+        print e
 
-def pull_messages(client, sub_name):
+def pull_messages(client, proj_name, sub_name, callback_fun):
     subscription = get_full_subscription_name(PROJECT_NAME, sub_name)
     
     while True:
@@ -53,35 +59,30 @@ def pull_messages(client, sub_name):
                 'returnImmediately': False,
                 'maxMessages': BATCH_SIZE
             }
-
             resp = client.projects().subscriptions().pull(
                     subscription=subscription, body=body).execute(
                     num_retries=NUM_RETRIES)
+
+            receivedMessages = resp.get('receivedMessages')
+            if receivedMessages:
+                ack_ids = []
+                for msg in receivedMessages:
+                    ack_ids.append(msg.get('ackId'))
+                    
+                    message = msg.get('message')
+                    if message:
+                        callback_fun(message)
+
+                ack_body = {'ackIds': ack_ids}
+                client.projects().subscriptions().acknowledge(
+                        subscription=subscription, body=ack_body).execute(num_retries=NUM_RETRIES)
         except Exception as e:
             time.sleep(0.5)
             print e
-            continue
         except KeyboardInterrupt:
             print "shutdown requested, exiting... "
             break
         
-        receivedMessages = resp.get('receivedMessages')
-        if receivedMessages:
-            ack_ids = []
-            for msg in receivedMessages:
-                ack_ids.append(msg.get('ackId'))
-
-                message = msg.get('message')
-                if message:
-                    attributes = message.get('attributes')
-                    if attributes:
-                        pp.pprint(attributes)
-
-#                   print base64.b64decode(str(message.get('data')))
-
-            ack_body = {'ackIds': ack_ids}
-            client.projects().subscriptions().acknowledge(
-                    subscription=subscription, body=ack_body).execute(num_retries=NUM_RETRIES)
 
 
 if __name__ == '__main__':
@@ -90,4 +91,25 @@ if __name__ == '__main__':
         credentials = credentials.create_scoped(PUBSUB_SCOPES)
 
     client = discovery.build('pubsub', 'v1', credentials=credentials)
-    pull_messages(client, 'pull_bucket_ven-custs')
+
+    def pprint_attr(message):
+        pp = pprint.PrettyPrinter(indent=4)
+
+        attributes = message.get('attributes')
+        if attributes:
+            pp.pprint(attributes)
+            print base64.b64decode(str(message.get('data')))
+    
+
+    pull_messages(client, PROJECT_NAME, 'pull_bucket_ven-custs', pprint_attr)
+
+    msg = {
+            'attributes': 
+            {
+                'bucketId': 'ven-cust-hohappy',
+                'eventType': 'OBJECT_FINALIZE'
+            },
+            'data':'ewogICJraW5kIjogInN0b3JhZ2Ujb2JqZWN0...'
+    }
+#    publish_message(client, PROJECT_NAME, TOPIC, msg)
+
