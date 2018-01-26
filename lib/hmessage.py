6097790599fd5099c7a64ff23e0cca2154a30e4f
eric0329 <ericyang0329@cron-1.c.venraasitri.internal>
2018-01-26 10:39:22 +0800

    [lib/hmessage.py] replace Tab to 4 spaces

--- lib/hmessage.py
+++ lib/hmessage.py
@@ -5,22 +5,25 @@ class HMessage() :
 
     def get_codename(self) :
         codename = ''
+        attr = self.msg.attributes
 
-	attr = self.msg.attributes
         if 'codename' in attr:
             codename = attr['codename']
         elif 'eventType' in attr:
-	    event_type = attr['eventType']
-	    if 'OBJECT_FINALIZE' == event_type:
-		bucket_id = attr['bucketId'] if 'bucketId' in attr else ''
-                codename = bucket_id.split('-')[-1]
-                attr['codename'] = codename
+            event_type = attr['eventType']
+
+        if 'OBJECT_FINALIZE' == event_type:
+            bucket_id = attr['bucketId'] if 'bucketId' in attr else ''
+            codename = bucket_id.split('-')[-1]
+            attr['codename'] = codename
 
         return codename
 
     def get_eventType(self) :
-	attr = self.msg.attributes
+        attr = self.msg.attributes
+
         return attr['eventType'] if 'eventType' in attr else ''
 
     def __str__(self) :
+
         return str(self.msg)
