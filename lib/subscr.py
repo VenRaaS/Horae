a4612e2d75a4bf3169f114888a818abcf67ddb90
eric0329 <ericyang0329@cron-1.c.venraasitri.internal>
2018-01-31 18:19:18 +0800

    [subscr.py] enrich EnumSubscript

--- lib/subscr.py
+++ lib/subscr.py
@@ -1,6 +1,5 @@
 from enum import Enum
 
 #-- Enum - https://stackoverflow.com/questions/36932/how-can-i-represent-an-enum-in-python?page=1&tab=votes#tab-top
-EnumSubscript = Enum('EnumSubscript', 'pull_bucket_ven-custs')
-
+EnumSubscript = Enum('EnumSubscript', 'pull_bucket_ven-custs pull_bigquery pull_es_cluster')
 
