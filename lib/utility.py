9fb5bd1511371792483f58799a96007686ea4cb3
eric0329 <ericyang0329@cron-1.c.venraasitri.internal>
2018-02-05 23:31:46 +0800

    [utility.ph] - replace double quote with a single space, remove_dq2space() - replace 0000-00-00 as an empty strting, remove_zero_datetime()

--- lib/utility.py
+++ lib/utility.py
@@ -1,17 +1,24 @@
 import sys
 import csv
-
+import subprocess
 from logger import logger
 
 
-def has_header(fn):
-    try:
-        with open(fn, 'rb') as f:
-            reader = csv.reader(f, delimiter='\t')
-            row1 = next(reader)
-            return not any(c.isdigit() for c in row1)
-    except Exception as e:
-        logger.error(e, exc_info=True)
+def has_header(ffn):
+    with open(ffn, 'rb') as f:
+        reader = csv.reader(f, delimiter='\t')
+        row1 = next(reader)
+        return not any(c.isdigit() for c in row1)
+
+def remove_dq2space(ffn):
+    cmd = "sed -i 's/\"/ /g' {}".format(ffn)
+    logger.info(cmd)
+    subprocess.call([cmd], shell=True)
+
+def remove_zero_datetime(ffn):
+    cmd = "sed -i 's/0000-00-00[^\t]*//g' {}".format(ffn)
+    logger.info(cmd)
+    subprocess.call([cmd], shell=True)
 
 
 if __name__ == '__main__':
