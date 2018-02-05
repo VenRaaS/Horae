a4ce2031749c576bd5140e4eae39a092d7441b6f
eric0329 <ericyang0329@cron-1.c.venraasitri.internal>
2018-02-06 00:00:44 +0800

    [ImportGOCC2bq.py] import with BQ schema autodetect - insert default HEADER into first row in tsv if there HEADER doesn't find - apply SAFE_CAST to cast well-known fields - replace 0000-00-00 with empty string

--- plugin/ImportGOCC2bq.py
+++ plugin/ImportGOCC2bq.py
@@ -14,6 +14,7 @@ lib_path = os.path.realpath(os.path.join(file_path, os.pardir, 'lib'))
 if not lib_path in sys.path : sys.path.append(lib_path)
 from event import EnumEvent, EnumTopic
 from subscr import EnumSubscript
+import utility
 
 
 class ImportGOCC2bq(Task.Task):
@@ -21,6 +22,19 @@ class ImportGOCC2bq(Task.Task):
     LISTEN_SUBSCRIPTS = [ EnumSubscript['pull_bucket_ven-custs'] ]
     LISTEN_EVENTS = [ EnumEvent.OBJECT_FINALIZE ]
     PUB_TOPIC = EnumTopic.bigquery
+
+    HEADER_GOODS = 'GID	PGID	GOODS_NAME	GOODS_KEYWORD	GOODS_BRAND	GOODS_DESCRIBE	GOODS_SPEC	GOODS_IMG_URL	AVAILABILITY	CURRENCY	SALE_PRICE	PROVIDER	BARCODE_EAN13	BARCODE_UPC	FIRST_RTS_DATE	UPDATE_TIME'
+    HEADER_ORDERLIST = 'UID	ORDER_NO	SEQ	ORDER_DATE	GID	CURRENCY	SALE_PRICE	FINAL_PRICE	QTY	FINAL_AMT	PROMO_ID	AFFILIATE_ID	DC_PRICE	DELIVERY_TYPE	UPDATE_TIME'
+    HEADER_CATEGORY = 'CATEGORY_NAME	CATEGORY_CODE	P_CATEGORY_CODE	LE	UPDATE_TIME'
+    HEADER_CUSTOMER = 'UID	GENDER	B_DATE	COUNTRY	AREA_1	AREA_2	EDM_YN	EMAIL	UPDATE_TIME'
+    HEADER_GOODSCATECODE = 'GID	CATEGORY_CODE	LE	SORT	FUNC_TYPE	INSERT_DATE	DISPLAY_START_DATE	DISPLAY_END_DATE	UPDATE_TIME'
+
+    SQL2UNIMA_GOODS = 'SELECT SAFE_CAST(GID AS string) AS GID, SAFE_CAST(PGID as string) AS PGID, SAFE_CAST(AVAILABILITY AS string) AS AVAILABILITY, SAFE_CAST(SALE_PRICE AS string) AS SALE_PRICE, SAFE_CAST(PROVIDER as string) AS PROVIDER, SAFE_CAST(FIRST_RTS_DATE AS DATETIME) AS FIRST_RTS_DATE, SAFE_CAST(UPDATE_TIME as DATETIME) AS UPDATE_TIME,  * EXCEPT (GID, PGID, AVAILABILITY, SALE_PRICE, PROVIDER, FIRST_RTS_DATE, UPDATE_TIME) FROM {}.{}'
+    SQL2UNIMA_CATEGORY = 'SELECT SAFE_CAST(CATEGORY_CODE AS string) AS CATEGORY_CODE, SAFE_CAST(P_CATEGORY_CODE AS string) AS P_CATEGORY_CODE, SAFE_CAST(LE AS string) AS LE, SAFE_CAST(UPDATE_TIME AS DATETIME) AS UPDATE_TIME,  * EXCEPT (CATEGORY_CODE, P_CATEGORY_CODE, LE, UPDATE_TIME) FROM {}.{}'
+    SQL2UNIMA_GOODSCATECODE = 'SELECT SAFE_CAST(GID AS string) AS GID, SAFE_CAST(CATEGORY_CODE AS string) AS CATEGORY_CODE, SAFE_CAST(LE AS string) AS LE, SAFE_CAST(INSERT_DATE AS DATETIME) AS INSERT_DATE, SAFE_CAST(DISPLAY_START_DATE AS DATETIME) AS DISPLAY_START_DATE, SAFE_CAST(DISPLAY_END_DATE AS DATETIME) AS DISPLAY_END_DATE, SAFE_CAST(UPDATE_TIME AS DATETIME) AS UPDATE_TIME,  * EXCEPT (GID, CATEGORY_CODE, LE, INSERT_DATE, DISPLAY_START_DATE, DISPLAY_END_DATE, UPDATE_TIME) FROM {}.{}'
+    SQL2UNIMA_CUSTOMER = 'SELECT SAFE_CAST(UID AS string) AS UID, case when EDM_YN is true then "Y" else "N" END AS EDM_YN, SAFE_CAST(UPDATE_TIME AS DATETIME) AS UPDATE_TIME,  * EXCEPT (UID, EDM_YN, UPDATE_TIME) FROM {}.{}'
+    SQL2UNIMA_ORDERLIST = 'SELECT SAFE_CAST(UID AS string) AS UID, SAFE_CAST(SEQ AS string) AS SEQ, SAFE_CAST(ORDER_DATE AS DATETIME) AS ORDER_DATE, SAFE_CAST(GID AS string) AS GID, SAFE_CAST(SALE_PRICE AS FLOAT64) AS SALE_PRICE, SAFE_CAST(FINAL_PRICE AS FLOAT64) AS FINAL_PRICE, SAFE_CAST(FINAL_AMT AS FLOAT64) AS FINAL_AMT, SAFE_CAST(DC_PRICE AS FLOAT64) AS DC_PRICE, SAFE_CAST(UPDATE_TIME AS DATETIME) AS UPDATE_TIME,  * EXCEPT (UID, SEQ, ORDER_DATE, GID, SALE_PRICE, FINAL_PRICE, FINAL_AMT, DC_PRICE, UPDATE_TIME) FROM {}.{}'
+
 
     def exe(self, hmsg) :
         if hmsg.get_attributes() :
@@ -65,73 +79,114 @@ class ImportGOCC2bq(Task.Task):
                         
                         dataPath = os.path.join(unpackPath, 'data')
 
+                        #-- data filenames 
+                        dataFNs = [ os.path.basename(fn) for fn in os.listdir(dataPath) ]
+                        self.logger.info(dataFNs)
+
                         #-- check file format
-                        self.check_num_fields(dataPath)
-                        self.check_file_encoding(dataPath)
+                        if not self.check_num_fields(dataPath, dataFNs): return
+                        if not self.check_file_encoding(dataPath, dataFNs): return
 
-                        #-- replace Double Quote \" to Space
-                        dataFiles = os.path.join(dataPath, '*')
-                        cmd = "sed -i 's/\"/ /g' {}".format(dataFiles)
-                        self.logger.info(cmd)
-                        # subprocess - https://docs.python.org/2/library/subprocess.html#using-the-subprocess-module
-                        subprocess.call([cmd], shell=True)
+                        #-- data correction
+                        self.dataCorrection(dataPath, dataFNs)
 
+                        #-- insert Header 
+                        for fn in dataFNs :
+                            ffn = os.path.join(dataPath, fn)
+                            self.logger.info('%s has_header: %s', ffn, utility.has_header(ffn))
+
+                            if not utility.has_header(ffn):
+                                cmd = "sed -i '1s/^/{}\\n/' {}"
+                                if ffn.endswith('Goods.tsv'): 
+                                    cmd = cmd.format(ImportGOCC2bq.HEADER_GOODS, ffn)
+                                elif 'OrderList' in os.path.split(ffn)[-1]:
+                                    cmd = cmd.format(ImportGOCC2bq.HEADER_ORDERLIST, ffn)
+                                elif ffn.endswith('Category.tsv'):
+                                    cmd = cmd.format(ImportGOCC2bq.HEADER_CATEGORY, ffn)
+                                elif ffn.endswith('Customer.tsv'):
+                                    cmd = cmd.format(ImportGOCC2bq.HEADER_CUSTOMER, ffn)
+                                elif ffn.endswith('GoodsCateCode.tsv'):
+                                    cmd = cmd.format(ImportGOCC2bq.HEADER_GOODSCATECODE, ffn)
+
+                                self.logger.info(cmd)
+                                subprocess.call([cmd], shell=True)
                        
                         #-- copy to GCS
                         gsTmpFolder = '_'.join(['gocc', date])
                         gsDataPath = os.path.join('gs://', bucketId, 'tmp', gsTmpFolder)
+                        dataFiles = os.path.join(dataPath, '*')
                         cmd = 'gsutil cp {} {}'.format(dataFiles, gsDataPath)
                         self.logger.info(cmd)
                         subprocess.call(cmd.split(' '))
-                       
-                        gsPath_goods = os.path.join(gsDataPath, 'Goods.tsv')
-                        cmd = 'bq load --source_format=CSV --F=''\t'' --replace --max_bad_records=10 {}_unima.goods_{} {} GID:string,PGID:string,GOODS_NAME:string,GOODS_KEYWORD:string,GOODS_BRAND:string,GOODS_DESCRIBE:string,GOODS_SPEC:string,GOODS_IMG_URL:string,AVAILABILITY:string,CURRENCY:string,SALE_PRICE:string,PROVIDER:string,BARCODE_EAN13:string,BARCODE_UPC:string,FIRST_RTS_DATE:string,UPDATE_TIME:string'.format(codename, date, gsPath_goods)
-                        self.logger.info(cmd)
-                        subprocess.call(cmd.split(' '))
 
-                        gsPath_category = os.path.join(gsDataPath, 'Category.tsv')
-                        cmd = 'bq load --source_format=CSV --F=''\t'' --replace --max_bad_records=10 {}_unima.category_{} {} CATEGORY_NAME:string,CATEGORY_CODE:string,P_CATEGORY_CODE:string,LE:string,UPDATE_TIME:string'.format(codename, date, gsPath_category)
-                        self.logger.info(cmd)
-                        subprocess.call(cmd.split(' '))
+                        #-- load into BQ tmp dataset
+                        for fn in dataFNs:
+                            gsPath = os.path.join(gsDataPath, fn)
 
-                        gsPath_gcc = os.path.join(gsDataPath, 'GoodsCateCode.tsv')
-                        cmd = 'bq load --source_format=CSV --F=''\t'' --replace --max_bad_records=10 {}_unima.goods_cate_code_{} {} GID:string,CATEGORY_CODE:string,LE:string,SORT:string,FUNC_TYPE:string,INSERT_DATE:string,DISPLAY_START_DATE:string,DISPLAY_END_DATE:string,UPDATE_TIME:string'.format(codename, date, gsPath_gcc)
-                        self.logger.info(cmd)
-                        subprocess.call(cmd.split(' '))
+                            baseName = os.path.splitext(fn)[0]
+                            tmpTb = 'ext_{}'.format(baseName.lower())
+                            dataset = '{}_tmp'.format(codename)
 
-                        dt = datetime.datetime.strptime(date, '%Y%m%d')
-                        yest_date = (dt.date() - datetime.timedelta(days=1)).strftime('%Y%m%d')
-                        gsPath_order = os.path.join(gsDataPath, 'OrderList_{}.tsv'.format(yest_date))
-                        cmd = 'bq load --source_format=CSV --F=''\t'' --replace --max_bad_records=10 {}_unima.orderlist_{} {} UID:string,ORDER_NO:string,SEQ:string,ORDER_DATE:DATETIME,GID:string,CURRENCY:string,SALE_PRICE:float,FINAL_PRICE:float,QTY:integer,FINAL_AMT:float,PROMO_ID:string,AFFILIATE_ID:string,DC_PRICE:float,DELIVERY_TYPE:string,UPDATE_TIME:DATETIME'.format(codename, yest_date, gsPath_order)
-                        self.logger.info(cmd)
-                        subprocess.call(cmd.split(' '))
+                            cmd = 'bq load --autodetect --replace --source_format=CSV --field_delimiter=''\t'' {}.{} {}'.format(dataset, tmpTb, gsPath)
+                            self.logger.info(cmd)
+                            subprocess.call(cmd.split(' '))
+
                         
+ 
+                        #-- load into BQ tmp dataset
+                        for fn in dataFNs:
+                            tmpDS = '{}_tmp'.format(codename)
+                            baseName = os.path.splitext(fn)[0]
+                            tmpTb = 'ext_{}'.format(baseName.lower())
+                            self.logger.info(tmpTb)
+
+                            unimaDS = '{}_unima'.format(codename)
+                            unimaTb = '{}_{}'.format(baseName.lower(), date)
+
+                            if tmpTb.endswith('goods'):
+                                sql = ImportGOCC2bq.SQL2UNIMA_GOODS.format(tmpDS, tmpTb)
+                            elif 'orderlist' in tmpTb:
+                                sql = ImportGOCC2bq.SQL2UNIMA_ORDERLIST.format(tmpDS, tmpTb)
+                            elif tmpTb.endswith('category'):
+                                sql = ImportGOCC2bq.SQL2UNIMA_CATEGORY.format(tmpDS, tmpTb)
+                            elif tmpTb.endswith('goodscatecode'):
+                                sql = ImportGOCC2bq.SQL2UNIMA_GOODSCATECODE.format(tmpDS, tmpTb)
+                            elif tmpTb.endswith('customer'):
+                                unimaTb = '{}'.format(baseName.lower())
+                                sql = ImportGOCC2bq.SQL2UNIMA_CUSTOMER.format(tmpDS, tmpTb)
+
+                            cmd = 'bq query -n 0 --replace --use_legacy_sql=False --destination_table={}.{} {}'.format(unimaDS, unimaTb, sql)
+                            self.logger.info(cmd)
+                            subprocess.call(cmd.split(' '))
+
                         #-- clean tmp folder in GCS 
                         cmd = 'gsutil rm -r -f {}'.format(gsDataPath)
                         self.logger.info(cmd)
-#                        subprocess.call(cmd.split(' '))
+                        subprocess.call(cmd.split(' '))
 
                         cmd = 'rm -rf {}'.format(unpackPath) 
                         subprocess.call(cmd.split(' '))
                         self.logger.info(cmd)
 
 
-    def check_file_encoding(self, dirPath) :
-        for fname in os.listdir(dirPath):
-            if fname.endswith('.csv') or fname.endswith('.tsv'):
+    def check_file_encoding(self, dirPath, dataFNs):
+        for fn in dataFNs:
+            if fn.endswith('.csv') or fn.endswith('.tsv'):
 
-                fpath = os.path.join(dirPath, fname) 
+                fpath = os.path.join(dirPath, fn) 
                 try:
                     with io.open(fpath, 'r', encoding='utf-8') as f :
                         f.readlines()
                 except UnicodeDecodeError:
                     self.logger.error(traceback.format_exc())
+                    return False
+        return True
 
-    def check_num_fields(self, dirPath) :
-        for fname in os.listdir(dirPath):
-            if fname.endswith('.csv') or fname.endswith('.tsv'):
+    def check_num_fields(self, dirPath, dataFNs):
+        for fn in dataFNs:
+            if fn.endswith('.csv') or fn.endswith('.tsv'):
 
-                fpath = os.path.join(dirPath, fname) 
+                fpath = os.path.join(dirPath, fn) 
                 with open(fpath, 'rb') as f :
                     reader = csv.reader(f, delimiter='\t')
                 
@@ -141,7 +196,17 @@ class ImportGOCC2bq(Task.Task):
                             num_fields_1st_row = len(fields)
                         else :
                              if len(fields) != num_fields_1st_row :
-                                 self.logger.error("line {} => {}, num of delimiters check failed!".format(reader.line_num, len(fields)))
+                                self.logger.error("line {} => {}, num of delimiters check failed!".format(reader.line_num, len(fields)))
+                                return False
+        return True
+    
+    def dataCorrection(self, dirPath, dataFNs):
+        for fn in dataFNs:
+            ffn = os.path.join(dirPath, fn)
+
+            utility.remove_dq2space(ffn)
+
+            utility.remove_zero_datetime(ffn)
 
 
 if '__main__' == __name__:
