f09089d0550cd06442059c858a99132f4eda032d
eric0329 <ericyang0329@cron-1.c.venraasitri.internal>
2018-02-21 17:43:19 +0800

    [subscr.py] add comments

--- lib/subscr.py
+++ lib/subscr.py
@@ -1,5 +1,10 @@
 from enum import Enum
 
-#-- Enum - https://stackoverflow.com/questions/36932/how-can-i-represent-an-enum-in-python?page=1&tab=votes#tab-top
+#-- Enum - aenum 
+#   see: 
+#       https://bitbucket.org/stoneleaf/aenum
+#       https://stackoverflow.com/questions/36932/how-can-i-represent-an-enum-in-python?page=1&tab=votes#tab-top
+
+##-- each Enum element is seprated by a SPACE
 EnumSubscript = Enum('EnumSubscript', 'pull_bucket_ven-custs pull_bigquery pull_es_cluster')
 
