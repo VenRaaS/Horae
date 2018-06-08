b80b3fd273d271b984bb590ab65c990230c2fbca
eric0329 <ericyang0329@gmail.com>
2018-06-08 16:51:33 +0800

    [add_alias_es.py] apply lib/* to publish message

--- add_alias_es.py
+++ add_alias_es.py
@@ -6,68 +6,29 @@ from datetime import date
 from googleapiclient import discovery
 from oauth2client.client import GoogleCredentials
 
-import lib.utility as util
-
-
-PUBSUB_SCOPES = ["https://www.googleapis.com/auth/pubsub"]
-NUM_RETRIES = 3
-BATCH_SIZE = 10
-
-
-def fqrn(resource_type, project, resource):
-    """Return a fully qualified resource name for Cloud Pub/Sub."""
-    return "projects/{}/{}/{}".format(project, resource_type, resource)
-
-def get_full_topic_name(project, topic):
-    """Return a fully qualified topic name."""
-    return fqrn('topics', project, topic)
-
-def publish_message(client, proj_name, topic, msg):
-    """Publish a message to a given topic."""
-    try:
-        topic = get_full_topic_name(proj_name, topic)
-
-        attributes = msg['attributes']
-        data = base64.b64encode(str(msg['data']))
-        body = {'messages': [
-                    {
-                        'attributes': attributes, 
-                        'data': data
-                    }
-                ]}
-
-        resp = client.projects().topics().publish(
-            topic=topic, body=body).execute(num_retries=NUM_RETRIES)
-
-        print ('Published a message "{}" to a topic {}. The message_id was {}.'
-               .format(msg['attributes'], topic, resp.get('messageIds')[0]))
-    except Exception as e:
-        print e
+from lib.hmessage import HMessage
+from lib.topic import EnumTopic
+from lib.event import EnumEvent
+import lib.pull_pub as pull_pub
 
 
 if __name__ == '__main__':
-    TOPIC = 'es-cluster'
-    CODENAME = 'crazymike'
-    INDEX = 'opp'
+    codename = 'crazymike'
+    index = 'opp'
     
     today = date.today().strftime('%Y%m%d')
-    index_es = '{cn}_{idx}_{d}'.format(cn=CODENAME, idx=INDEX, d=today)
-    msg = {
-            'attributes': 
-            {
-                'objectIds': json.dumps( [index_es] ),
-                'eventType': 'OBJECT_FINALIZE',
-                'codename': 'crazymike',
-                
-            },
-            'data':'ooxx...'
-    }
+    index_es = '{cn}_{idx}_{d}'.format(cn=codename, idx=index, d=today)
+    objIds = [index_es]
+
+    hmsg = HMessage()
+    hmsg.set_codename(codename)
+    hmsg.set_eventType(EnumEvent.OBJECT_FINALIZE)
+    hmsg.set_objectIds(objIds)
 
     credentials = GoogleCredentials.get_application_default()
     if credentials.create_scoped_required():
         credentials = credentials.create_scoped(PUBSUB_SCOPES)
     client = discovery.build('pubsub', 'v1', credentials=credentials)
-    publish_message(client, util.get_projectID(), TOPIC, msg)
 
-
+    pull_pub.publish_message(client, EnumTopic['es-cluster'], [hmsg])
 
