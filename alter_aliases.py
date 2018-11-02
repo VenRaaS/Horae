e69f52522bc0fca9643012c68c0749419c14444e
eric0329 <ericyang0329@gmail.com>
2018-11-02 15:42:32 +0800

    [alter_aliases.py] new added

---
+++ alter_aliases.py
@@ -0,0 +1,115 @@
+import logging
+import json
+import requests
+import datetime
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s [%(levelname)s] %(message)s', datefmt='%Y-%m-%d %I:%M:%S')
+
+LB_ES_HOSTS = '10.140.0.7'
+INDEX_CATS = ['oua', 'opp', 'mod', 'gocc']
+
+RESERVED_DAYS = {
+    'oua': 14,
+    'opp': 14, 
+    'gocc': 3,
+    'mod': 3,
+}
+
+URL_VENRAAS_COMS = 'http://{h}:9200/venraas/_search?sort=update_dt:desc&size=1'
+URL_DISTINCT_INDICES = 'http://{h}:9200/{cn}_{cat}_*/_search?size=0'
+JSON_DISTINCT_INDICES = {"aggs":{"index_list":{"terms":{"field":"_index","size":100}}}}
+
+URL_DELETE_INDICE = 'http://{h}:9200/{idx}'
+URL_COUNT_INDICE = 'http://{h}:9200/{idx}/_count'
+
+URL_ALIASES = 'http://{h}:9200/_aliases'
+JSON_ADD_RM_ALIAS = {"actions":[{"add":{"index":"{newidx}","alias":"{ali}"}},{"remove":{"index":"{oldidx}","alias":"{ali}"}}]}
+JSON_ADD_ALIAS = {"actions":[{"add":{"index":"{idx}","alias":"{cn}_{cat}"}}]}
+JSON_ADD_ALIASES = {"actions":[{"add":{"index":"{cn}_{cat}_*","alias":"{cn}_{cat}"}}]}
+
+
+
+
+if '__main__' == __name__:
+
+    url = URL_VENRAAS_COMS.format(h=LB_ES_HOSTS)
+    resp = requests.get(url)
+    raasJson = json.loads(resp.text)
+    coms  = raasJson['hits']['hits'][0]['_source']['companies']
+    for com in coms:
+        if not com['enabled'] and \
+           not com['code_name']:
+            continue
+
+        codename = com['code_name']
+        
+        resp = requests.get(URL_ALIASES.format(h=LB_ES_HOSTS))
+        iaInfoJson = json.loads(resp.text)
+ 
+        for cate in INDEX_CATS:
+            alias = '{cn}_{cat}'.format(cn=codename, cat=cate)
+            indices = filter(lambda k: alias in k, iaInfoJson)
+            if len(indices) <= 0:
+                logging.warn('none of indices with prefix {0}'.format(alias))
+                continue
+
+            indices.sort(reverse=True)
+            idx_latest = indices[0]
+            alias_latest = next((i for i in indices if iaInfoJson[i]['aliases']), None)
+
+            #-- alter aliases
+            if cate in ['oua', 'opp']:
+                if None == alias_latest or \
+                   alias_latest != idx_latest:
+                    JSON_ADD_ALIASES['actions'][0]['add']['index'] = '{cn}_{cat}_*'.format(cn=codename, cat=cate)
+                    JSON_ADD_ALIASES['actions'][0]['add']['alias'] = '{cn}_{cat}'.format(cn=codename, cat=cate)
+                    url = URL_ALIASES.format(h=LB_ES_HOSTS)
+                    resp = requests.post(url, json=JSON_ADD_ALIASES)
+                    logging.info('{0} --data {1}, {2}'.format(url, JSON_ADD_ALIASES, resp.text))
+                else:
+                    logging.info('{0}(idx) = {1}(alias), latest indices are equal, awesome +1'.format(idx_latest, alias_latest))
+            elif cate in ['mod', 'gocc']:
+                if None == alias_latest:
+                    # newborn, none alias yet
+                    logging.info('newborn, none alias yet')
+                    logging.info('let\'s alias the latest one {0} ...'.format(idx_latest))
+                    JSON_ADD_ALIAS['actions'][0]['add']['index'] = idx_latest
+                    JSON_ADD_ALIAS['actions'][0]['add']['alias'] = alias
+                    url = URL_ALIASES.format(h=LB_ES_HOSTS)
+                    resp = requests.post(url, json=JSON_ADD_ALIAS)
+                    logging.info('{0} --data {1}, {2}'.format(url, JSON_ADD_ALIAS, resp.text))
+                elif alias_latest != idx_latest:
+                    logging.info('{0}(idx) <> {1}(alias), latest indices is not equal'.format(idx_latest, alias_latest))
+                    logging.info('let\'s sync the alias to the latest index ...')
+                    url = URL_COUNT_INDICE.format(h=LB_ES_HOSTS, idx=idx_latest)
+                    resp = requests.get(url)
+                    cnt_idx = float(json.loads(resp.text)['count'])
+                    url = URL_COUNT_INDICE.format(h=LB_ES_HOSTS, idx=alias_latest)
+                    resp = requests.get(url)
+                    cnt_alias = float(json.loads(resp.text)['count'])
+                    ratio = min(cnt_idx,cnt_alias) / max(cnt_idx,cnt_alias)
+                    if 0.98 <= min(cnt_idx,cnt_alias) / max(cnt_idx,cnt_alias):
+                        logging.info('0.98 < {0}, valid ratio.'.format(ratio))
+                        # json command
+                        JSON_ADD_RM_ALIAS['actions'][0]['add']['index'] = idx_latest
+                        JSON_ADD_RM_ALIAS['actions'][0]['add']['alias'] = alias 
+                        JSON_ADD_RM_ALIAS['actions'][1]['remove']['index'] = alias_latest 
+                        JSON_ADD_RM_ALIAS['actions'][1]['remove']['alias'] = alias
+                        url = URL_ALIASES.format(h=LB_ES_HOSTS)
+                        resp = requests.post(url, json=JSON_ADD_RM_ALIAS)
+                        logging.info('{0} --data {1}, {2}'.format(url, JSON_ADD_RM_ALIAS, resp.text))
+                    else:
+                        logging.warn('{0}/{1}= {2} < 0.98 unable to alter alias due to invalid ratio.'.format( \
+                            min(cnt_idx,cnt_alias), max(cnt_idx,cnt_alias), ratio))
+                else:
+                    logging.info('{0}(idx) = {1}(alias), latest indices are equal, awesome +1'.format(idx_latest, alias_latest))
+                 
+
+#            #-- purge indices
+#            if RESERVED_DAYS[cate] < len(indices):
+#                for i in indices[ :len(indices) - RESERVED_DAYS[cate]]:
+#                    urldel = URL_DELETE_INDICE.format(h=LB_ES_HOSTS, idx=i)
+#                    resp = requests.delete(urldel)
+#                    logging.info('del {0}, {1}'.format(urldel, resp.text))
+ 
+            today = datetime.date.today().strftime('%Y%m%d')
