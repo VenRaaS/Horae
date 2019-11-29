fd1244439129bb0adf080cbb30cc0935f3ea955d
Robin Su <robin.rsu@gmail.com>
2019-11-29 11:10:06 +0800

    exec retargeting shell

--- plugin/ExecRetargetingShell.py
+++ plugin/ExecRetargetingShell.py
@@ -20,7 +20,7 @@ import plugin.Task as Task
 logger = logging.getLogger(__name__)
 
 
-class ExeceDMShell(Task.Task):
+class ExecRetargetingShell(Task.Task):
     INVOKE_INTERVAL_SEC = 60 * 5
     LISTEN_SUBSCRIPTS = [ EnumSubscript['pull_bucket_ven-custs'] ]
     LISTEN_EVENTS = [ EnumEvent.OBJECT_FINALIZE ]
@@ -33,43 +33,41 @@ class ExeceDMShell(Task.Task):
             attributes = hmsg.get_attributes()
             event_type = hmsg.get_eventType()
             
-            if EnumEvent[event_type] in ExeceDMShell.LISTEN_EVENTS:
+            if EnumEvent[event_type] in ExecRetargetingShell.LISTEN_EVENTS:
                 bucketId = attributes['bucketId'] if 'bucketId' in attributes else ''
                 objectId = attributes['objectId'] if 'objectId' in attributes else ''
                 generation = attributes['objectGeneration'] if 'objectGeneration' in attributes else ''
                 logger.info('%s %s %s %s', event_type, bucketId, objectId, generation)
-                
                 gsPaths = objectId.split('/')
-                logger.info('%s',gsPaths)
-                if 4 == len(gsPaths) and gsPaths[0] == 'retargeting' and gsPaths[1] == 'edm' :
+                if 4 == len(gsPaths) and gsPaths[0] == 'retargeting' and gsPaths[1] == 'edm' and gsPaths[2] == 'input' :
                     #subprocess.call(["sh", "/home/itri/angel/process.sh"])
                     logger.info('%s',objectId[-19:])
                     m = re.match(r'edm_(\d{8})\.tar\.gz$', objectId[-19:])
                     #logger.info('%s',m)
                     if m:
-                       subprocess.call(["sh", "/home/itri/angel/process.sh", gsPaths[3]])
-                elif 4 == len(gsPaths) and gsPaths[0] =='retargeting' and gsPaths[1] == 'user_clustering' :
+                       subprocess.call(["sh", "/home/itri/angel/edm_process.sh", gsPaths[3]])
+                elif 4 == len(gsPaths) and gsPaths[0] =='retargeting' and gsPaths[1] == 'user_clustering' and gsPaths[2] == 'input' :
                     #subprocess.call(["sh", "/home/itri/angel/process.sh"])
                     logger.info('%s',objectId[-18:])
                     m = re.match(r'uc_(\d{8})\.tar\.gz$', objectId[-18:])
                     #logger.info('%s',m)
                     if m:
-                       subprocess.call(["sh", "/home/itri/angel/process.sh", gsPaths[3]])
-                elif 4 == len(gsPaths) and gsPaths[0] =='retargeting' and gsPaths[1] == 'upg' :
+                       subprocess.call(["sh", "/home/itri/angel/uc_process.sh", gsPaths[3]])
+                elif 4 == len(gsPaths) and gsPaths[0] =='retargeting' and gsPaths[1] == 'upg' and gsPaths[2] == 'input':
                     #subprocess.call(["sh", "/home/itri/angel/process.sh"])
                     logger.info('%s',objectId[-19:])
-                    m = re.match(r'uc_(\d{8})\.tar\.gz$', objectId[-19:])
+                    m = re.match(r'upg_(\d{8})\.tar\.gz$', objectId[-19:])
                     #logger.info('%s',m)
                     if m:
-                       subprocess.call(["sh", "/home/itri/angel/process.sh", gsPaths[3]])
+                       subprocess.call(["sh", "/home/itri/angel/upg_process.sh", gsPaths[3]])
                 
 
 if '__main__' == __name__:
     class MockMsg() :
         def __init__(self):
-           self.message = {'ExeceDMShell':{'eventType':'OBJECT_FINALIZE', 'objectId':'fake message'}}
+           self.message = {'ExecRetargetingShell':{'eventType':'OBJECT_FINALIZE', 'objectId':'fake message'}}
     
     hmsg = HMessage(MockMsg().message)  
-    t = ExeceDMShell(hmsg)
+    t = ExecRetargetingShell(hmsg)
    # t.start()
    # t.join()
