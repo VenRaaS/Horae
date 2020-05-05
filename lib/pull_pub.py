7364fc61015297d416b82f3b717802165792feeb
eric0329 <ericyang0329@gmail.com>
2020-05-05 23:17:55 +0800

    [pull_pub.py] logging

--- lib/pull_pub.py
+++ lib/pull_pub.py
@@ -7,7 +7,7 @@ import lib.utility as util
 from lib.hmessage import HMessage
 from lib.event import EnumEvent
 
-logger = logging.getLogger(__name__)
+###logger = logging.getLogger(__name__)
 
 PUBSUB_SCOPES = ["https://www.googleapis.com/auth/pubsub"]
 NUM_RETRIES = 3
@@ -19,7 +19,7 @@ def pull_messages(client, sub_enum, callback_fn):
     subscription = util.get_full_subscription_name(proj_name, sub_enum.name)
     
     try:
-        logger.info("pull from {} ...".format(subscription))
+        logging.info("pull from {} ...".format(subscription))
 
         #--  pulls messages from the server
         #    see https://cloud.google.com/pubsub/docs/reference/rest/v1/projects.subscriptions/pull
@@ -32,7 +32,7 @@ def pull_messages(client, sub_enum, callback_fn):
                 num_retries=NUM_RETRIES)
 
         receivedMessages = resp.get('receivedMessages')
-#        logger.info(receivedMessages)
+#        logging.info(receivedMessages)
         if receivedMessages:
             ack_ids = []
             for msg in receivedMessages:
@@ -40,17 +40,18 @@ def pull_messages(client, sub_enum, callback_fn):
                 
                 message = msg.get('message')
                 if message:
+#                    pass
                     callback_fn(message)
 
             ack_body = {'ackIds': ack_ids}
-            logger.info(ack_body)
+            logging.info(ack_body)
             client.projects().subscriptions().acknowledge(
                     subscription=subscription, body=ack_body).execute(num_retries=NUM_RETRIES)
     except Exception as e:
-        logger.error(e, exc_info=True)
+        logging.error(e, exc_info=True)
         time.sleep(0.5)
     except KeyboardInterrupt:
-        logger.info("shutdown requested, exiting... ")
+        logging.info("shutdown requested, exiting... ")
 
 
 ##
@@ -60,7 +61,7 @@ def publish_message(client, topic_enum, hmsgs):
     try:
         proj_name = util.get_projectID()
         topic = util.get_full_topic_name(proj_name, topic_enum.name)
-        logger.info('topic: {}'.format(topic))
+        logging.info('topic: {}'.format(topic))
 
         #-- concate message list
         #   note that the client.projects().subscriptions().pull(...) gets message seperately, e.g. len(receivedMessages) = 1, even if 1 < len(hmsgs)
@@ -79,9 +80,9 @@ def publish_message(client, topic_enum, hmsgs):
 
         resp = client.projects().topics().publish(topic=topic, body=body).execute(num_retries=NUM_RETRIES)
 
-        logger.info('Published a message "{}" to a topic {}. The message_id was {}.'.format(body, topic, resp.get('messageIds')[0]))
+        logging.info('Published a message "{}" to a topic {}. The message_id was {}.'.format(body, topic, resp.get('messageIds')[0]))
     except Exception as e:
-        logger.error(e, exc_info=True)
+        logging.error(e, exc_info=True)
 
  
 if '__main__' == __name__ :
@@ -90,7 +91,7 @@ if '__main__' == __name__ :
     hmsg.set_codename('gohappy')
     hmsg.set_eventType(EnumEvent.OBJECT_FINALIZE)
     hmsg.set_objectIds(msgObjs)
-    logger.info(hmsg)
+    logging.info(hmsg)
     pull_pub.publish_message(client, EnumTopic.bigquery, [hmsg])
     
     msgObjs = ['titantech_gocc_20180309']
@@ -98,5 +99,5 @@ if '__main__' == __name__ :
     hmsg.set_codename('titantech')
     hmsg.set_eventType(EnumEvent.OBJECT_FINALIZE)
     hmsg.set_objectIds(msgObjs)
-    logger.info(hmsg)
+    logging.info(hmsg)
     pull_pub.publish_message(client, EnumTopic['es-cluster'], [hmsg])
